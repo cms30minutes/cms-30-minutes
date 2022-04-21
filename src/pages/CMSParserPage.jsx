8d153d7a3c9279971a4cc3be8cb04fc7a16dd07d
Øystein Malt <me@oysmal.com>
2022-04-21 07:42:53 +0200

    fix link styles

--- src/pages/CMSParserPage.jsx
+++ src/pages/CMSParserPage.jsx
@@ -72,7 +72,7 @@ function createImage(item) {
 }
 
 function createLink(item) {
-  return <a className="my-2" href={item.Link}>{item.Content}</a>;
+  return <div><a className="my-2 underline color-blue-500" href={item.Link}>{item.Content}</a></div>;
 }
 
 function createSpacer(item) {
